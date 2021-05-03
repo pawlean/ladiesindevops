e1da5151b7fc847da3cd5aed1609cafc18a6b35e
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2021-05-03 14:53:26 +0000

    Update sponsorship page

--- pages/sponsorship.js
+++ pages/sponsorship.js
@@ -3,7 +3,39 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-        DevOps
+        <h1>Thank you for your interest in sponsoring the “Ladies in DevOps” community!</h1>
+        <p>Ladies in DevOps started in April 2021 with one mission in mind: to bring together self-identifying women who either work in the DevOps space or are looking to explore the exciting career opportunities. We currently have over 400 members from across the world all with various degrees of experience (from students, early career professionals to managers!) </p>
+        <p>
+We really appreciate your support in our community. Here are ways that you can get involved and support us:
+</p>
+<h2>Paid support</h2>
+<h3>1. Have a dedicated channel on our Discord </h3><p>
+This will be a dedicated space for you to hang out with our community and share opportunities e.g. events/workshops that your company is holding or jobs that you’d like to advertise.
+</p>
+
+<h3>2. Fund learning & development opportunities</h3><p>
+In this support tier, you’ll be able to fund a course or resource for our community members to thrive in their journey in the DevOps space. This resource can be of your choosing or you can send us funds directly to purchase courses/resources for the folks that need it most. Examples: O'Reilly books, Udemy courses, Tech World With Nana courses, ACloudGuru membership.
+</p>
+<h3>3. Sponsor a giveaway!</h3><p>
+You can support us by sponsoring a giveaway! This can be anything of your choice, examples include learning resources, tech (e.g. headphones, laptops, keyboards etc.), things to help with WFH and more. Your choice to be creative and give back to our community! 
+</p>
+<h3>4. Sponsor our community events (coming soon)</h3>
+<p>
+We’re hoping to organise some community events within our Discord server. You can support this by sponsoring the event which gives you a dedicated channel on our Discord and your logo on event presentations and other assets (videos, tweets etc.) across social media. 
+</p>
+Free support
+<h3>1. Share our community internally</h3>
+<p>If there are any groups in your company that you think would benefit from being part of Ladies in DevOps - please do share!</p>
+
+<h3>2. Share our community on social media</h3>
+<p>We share posts across our social media accounts - Twitter, Instagram and LinkedIn - spreading the word and sharing our posts is a good way to reach people who may not even know of the exciting career opportunities that are available in the DevOps space. Sharing will help us a lot!
+
+</p>
+
+<p>Thank you once again for your interest in helping our community! 
+
+
+</p>
     </div>
   );
 }
