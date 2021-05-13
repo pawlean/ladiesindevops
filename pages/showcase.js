531c8e9188196b8584505aa113a7dbc6a53e4de2
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2021-05-13 19:31:19 +0000

    Update showcase

--- pages/showcase.js
+++ pages/showcase.js
@@ -9,8 +9,9 @@ export default function Home() {
                 <h1>🏆 Showcase</h1>
                 <p>
                     <ul>
-                        <li><a href="https://devxconf.org/expo" target="_blank">DevX Conf</a></li>
-                        <li><a href="https://www.thedevcoach.co.uk/best-online-cloud-communities/" target="_blank">Open Up The Cloud</a></li>
+                        <li><a href="https://devxconf.org/expo" target="_blank">DevX Conf - Partner Exposition</a></li>
+                        <li><a href="https://www.thedevcoach.co.uk/best-online-cloud-communities/" target="_blank">Open Up The Cloud Article - Best Online Communities</a></li>
+                        <li><a href="https://www.eddiehub.org/?r_done=1" target="_blank">EddieHub Community Partner</a></li>
                     </ul>
                 </p>
             </main>
