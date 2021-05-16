ba7809aa39c666d1cb97c1acbe4804770054dca1
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2021-05-16 11:14:08 +0000

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -4,43 +4,33 @@ function MyApp({ Component, pageProps }) {
   <Head>
     <title>Ladies in DevOps</title>
     <link rel="icon" href="/favicon.ico" />
-
     <meta
       property="og:title"
       content="Ladies in DevOps - Join the community today!"
-      key="title"
     />
-    <meta property="og:site_name" content="Ladies in DevOps" key="title" />
-    <meta property="og:url" content="https://ladiesindevops.com" key="title" />
+    <meta property="og:site_name" content="Ladies in DevOps" />
+    <meta property="og:url" content="https://ladiesindevops.com" />
     <meta
       property="og:description"
       content="Join a community for women excelling in their DevOps careers!"
-      key="title"
     />
-    <meta property="og:type" content="website" key="title" />
+    <meta property="og:type" content="website" />
     <meta
       property="og:image"
       content="https://ladiesindevops.com/LadiesInDevOpsSocial.png"
-      key="title"
     />
 
-    <meta name="twitter:card" content="summary_large_image" key="title" />
-    <meta
-      name="twitter:site"
-      content="https://ladiesindevops.com"
-      key="title"
-    />
-    <meta name="twitter:creator" content="@ladiesindevops" key="title" />
-    <meta name="twitter:title" content="Ladies in DevOps" key="title" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="https://ladiesindevops.com" />
+    <meta name="twitter:creator" content="@ladiesindevops" />
+    <meta name="twitter:title" content="Ladies in DevOps" />
     <meta
       name="twitter:description"
       content="Join a community for women excelling in their DevOps careers!"
-      key="title"
     />
     <meta
       name="twitter:image"
       content="https://ladiesindevops.com/LadiesInDevOpsSocial.png"
-      key="title"
     />
   </Head>;
   return <Component {...pageProps} />;
